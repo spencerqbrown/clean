fa0946c4968f57b6cc5de2c4d768cdd249a79528
spencerqbrown <49326312+spencerqbrown@users.noreply.github.com>
2020-12-01 13:16:51 -0600

    wrote and executed evaluator function, cleaned its results into unscraped dataset, now working on rescraping missing locations

--- evaluate.py
+++ evaluate.py
@@ -19,7 +19,7 @@ import pandas as pd
 from selenium import webdriver
 from scroll import pauseScroll
 
-def get_review_counts(data_path, id_col, wait=[1,1.5], url_col=None, search_terms=None, new_url_col=None, review_col_name="review_count"):
+def get_review_counts(data_path, id_col, output_path, wait=[1,1.5], url_col=None, search_terms=None, new_url_col=None, review_col_name="review_count"):
     df = pd.read_csv(data_path)
 
     # if necessary, build urls
@@ -37,7 +37,7 @@ def get_review_counts(data_path, id_col, wait=[1,1.5], url_col=None, search_term
     # get relevant columns as lists
     urls = df[url_col]
     ids = df[id_col]
-    counts = []
+    counts = [None]*len(ids)
 
     for i in range(len(urls)):
         # get url
@@ -75,18 +75,48 @@ def get_review_counts(data_path, id_col, wait=[1,1.5], url_col=None, search_term
     id_review_dict = {id_col:ids, review_col_name:counts}
     out_df = pd.DataFrame.from_dict(id_review_dict)
 
+    driver.quit()
+    out_df.to_csv(output_path, index=False)
+
     return out_df
 
-def evaluate_locations(data_path, scraped_df, output_path, data_id_col, scraped_id_col, data_count_col, wait=[1,1.5], url_col=None, search_terms=None, new_url_col=None, review_col_name="review_count", diff_col="difference", prop_col="proportion"):
-    count_df = get_review_counts(data_path, data_id_col, wait, url_col, search_terms, new_url_col, review_col_name)
-    scraped_df = pd.read_csv(scraped_df)
-    out_df = count_df.join(scraped_df, on=(data_id_col, scraped_id_col))
-    out_df[diff_col] = out_df[data_count_col] - out_df[review_col_name]
-    out_df[prop_col] = out_df[data_count_col] / out_df[review_col_name]
-    return out_df
+def evaluate_locations(data_path, scraped_path, output_path, counts_output_path, id_col, data_count_cols, wait=[1,1.5], url_col=None, search_terms=None, new_url_col=None, scraped_col_name="reviews_scraped", review_col_name="review_count", diff_col="difference", prop_col="proportion"):
+    count_df = get_review_counts(data_path, id_col, counts_output_path, wait, url_col, search_terms, new_url_col, review_col_name)
+    scraped_df = pd.read_csv(scraped_path)
+    out_df = pd.merge(count_df, scraped_df, on=[id_col])
+
+    total_review_count = out_df[data_count_cols].sum(axis=1)
+
+    out_df[diff_col] = total_review_count - out_df[review_col_name]
+    out_df[prop_col] = total_review_count / out_df[review_col_name]
+    out_df[scraped_col_name] = total_review_count
+    out_df.to_csv(output_path, index=False)
 
 
 def raw_review_count_to_int(raw_review_count):
     return int(raw_review_count.split(" ")[0].replace(",",""))
 
+from utilities import get_files
+
+def evaluate_all(data_path, scraped_directory_path, output_directory_path, counts_output_path, id_col, data_count_cols, wait=[1,1.5], url_col=None, search_terms=None, new_url_col=None, scraped_col_name="reviews_scraped", review_col_name="review_count", diff_col="difference", prop_col="proportion"):
+    count_df = get_review_counts(data_path, id_col, counts_output_path, wait, url_col, search_terms, new_url_col, review_col_name)
+
+    for file in get_files(scraped_directory_path, "csv"):
+
+        scraped_path = file
+        scraped_df = pd.read_csv(scraped_path)
+        out_df = pd.merge(count_df, scraped_df, on=[id_col])
+
+        total_review_count = out_df[data_count_cols].sum(axis=1)
+
+        out_df[diff_col] = total_review_count - out_df[review_col_name]
+        out_df[prop_col] = total_review_count / out_df[review_col_name]
+        out_df[scraped_col_name] = total_review_count
+
+        out_df = out_df[[id_col, scraped_col_name, diff_col, prop_col, review_col_name]]
+
+        filename = file.split("\\")[-1].split(".")[0]
+        output_path = output_directory_path + "\\" + filename + "_evaluation.csv"
+        out_df.to_csv(output_path, index=False)
+        print("Saved file as " + output_path)
 
